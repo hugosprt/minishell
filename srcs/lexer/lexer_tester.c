efdffd5e20135df43e3d7881ae9e9bc97a2f25c5
Ubuntu <ubuntu@ip-172-31-31-104.eu-west-3.compute.internal>
2022-09-05 09:20:27 +0000

    gestion d'erreur

--- srcs/lexer/lexer_tester.c
+++ srcs/lexer/lexer_tester.c
@@ -15,27 +15,26 @@ int	size_list(t_lexer *s)
 	return (count);
 }
 
-int	first_arg(t_lexer *l)
+int	first_arg(t_shell *s, t_lexer *l)
 {
 	t_lexer	*tmp;
 
 	tmp = l;
 	if (tmp->koi == PIPE && (tmp->next->koi == ARG || tmp->next->koi == PIPE))
-		return (printf("syntax error\n"), 0);
+		syntax_problem(s, 0);
 	if (size_list(tmp) == 2)
 	{
 		if (tmp->koi > 0)
-			return (printf("syntax error\n"), 0);
+			syntax_problem(s, 5);
 	}
 	else if (tmp->koi == PIPE && (tmp->next->koi == ARG || tmp->next->koi == PIPE))
-		return (printf("syntax error\n"), 0);
+		syntax_problem(s, 0);
 	else
 	{
 		tmp = tmp->next;
-		while (tmp && tmp->next)
+		while (tmp && tmp->next && !s->error)
 		{
-			if (!test_pipe(tmp))
-				return (printf("syntax error\n"), 0);
+			test_pipe(s, tmp);
 			tmp = tmp->next;
 		}
 	}
@@ -74,30 +73,28 @@ t_lexer	*find_last_elem2(t_lexer *l)
 	return (NULL);
 }
 
-int 	test_pipe(t_lexer *l)
+int 	test_pipe(t_shell *s, t_lexer *l)
 {
 	t_lexer	*tmp;
 
 	tmp = l;
 	if (find_last_elem(tmp) == PIPE)
-		return (0);
+		syntax_problem(s, 0);
 	if (tmp->prev->koi == R_REDIR && tmp->koi == PIPE)
-		return (0);
+		syntax_problem(s, 0);
 	if (tmp->koi == L_REDIR && tmp->next->koi == END)
-		return (0);
+		syntax_problem(s, 5);
 	if (tmp->prev->koi == L_REDIR && tmp->koi == PIPE)
-		return (0);
+		syntax_problem(s, 0);
 	if (tmp->prev->koi == RR_REDIR && tmp->koi == PIPE && tmp->next->koi == PIPE)
-		return (0);
-	if (tmp->prev->koi >= R_REDIR && tmp->koi == PIPE && tmp->next->koi == END)
-		return (0);
+		syntax_problem(s, 0);
 	if (tmp->koi == PIPE && tmp->next->koi == PIPE)
 		return (0);
 	if (tmp->koi == LL_REDIR && tmp->next->koi == END)
-		return (0);
+		syntax_problem(s, 5);
 	if (tmp->koi == R_REDIR && tmp->next->koi == END)
-		return (0);
+		syntax_problem(s, 5);
 	if (tmp->koi == RR_REDIR && tmp->next->koi == END)
-		return (0);
+		syntax_problem(s, 5);
 	return (1);
 }
