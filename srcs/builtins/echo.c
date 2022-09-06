a8d28eea307d1d42822e223a028a5f625f1d3fc9
Ubuntu <ubuntu@ip-172-31-31-104.eu-west-3.compute.internal>
2022-09-06 14:51:47 +0000

    fix echo

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -1,55 +1,131 @@
 #include "../../includes/minishell.h"
 
-int	ft_strcmp(char *s1, char *s2)
+// int	ft_strcmp(char *s1, char *s2)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+// 	{
+// 		i++;
+// 		if (i == 2)
+// 			return (s1[i - 1] - s2[i - 1]);
+// 	}
+// 	return (s1[i] - s2[i]);
+// }
+
+// int	is_n(char *str, int n)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	if (str[0] == '-' && str[1] == 'n')
+// 	{
+// 		i++;
+// 		while (str[i] == 'n')
+// 			i++;
+// 		if (str[i] != ' ' && str[i] != '\0')
+// 			return (n);
+// 		while (str[i] == ' ')
+// 			i++;
+// 	}
+// 	if (str[i] == '-' && str[i + 1] == 'n')
+// 		i = is_n((str + i), i + n);
+// 	return (i + n);
+// }
+
+// void	echo(t_shell *sh)
+// {
+// 	t_parsing	*p;
+// 	int			n;
+// 	char		*str;
+
+// 	p = sh->parsing;
+// 	str = p->arg[1];
+// 	n = 0;
+// 	if (str)
+// 	{
+// 		n = is_n(str, 0);
+// 		ft_putstr_fd((str + n), 1);
+// 	}
+// 	if (n == 0)
+// 		ft_putstr_fd("\n", 1);
+// 	s()->sig->ret = 0;
+// 	return ;
+// }
+
+int		ft_strcmp(char *s1, char *s2)
 {
-	int	i;
+	int i;
 
 	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
 	{
 		i++;
 		if (i == 2)
-			return (s1[i - 1] - s2[i - 1]);
+			return (s1[i-1] - s2[i-1]);
+		
 	}
 	return (s1[i] - s2[i]);
 }
 
-int	is_n(char *str, int n)
+int is_n(char *str)
 {
-	int	i;
+	
+	int a;
+	int i;
 
 	i = 0;
-	if (str[0] == '-' && str[1] == 'n')
+	if (!str)
+		return (0);
+	if (str[0] != '-')
+	{
+		return (0);
+	}
+	else
 	{
 		i++;
-		while (str[i] == 'n')
-			i++;
-		if (str[i] != ' ' && str[i] != '\0')
-			return (n);
-		while (str[i] == ' ')
-			i++;
+		a = i;
+		while (str[i])
+		{
+			if (str[i] != 'n' || !str[i])
+				return (0);
+			i++;	
+		}
+		if (i == a)
+			return (0);
 	}
-	if (str[i] == '-' && str[i + 1] == 'n')
-		i = is_n((str + i), i + n);
-	return (i + n);
+	return (1);	
 }
 
-void	echo(t_shell *sh)
+void echo(t_shell *shell)
 {
-	t_parsing	*p;
-	int			n;
-	char		*str;
+	int i;
+	int n;
+	char **str;
 
-	p = sh->parsing;
-	str = p->arg[1];
-	n = 0;
-	if (str)
+	str = shell->parsing->arg;
+	i = 0;
+	n = 1;
+	i = 1;
+	if (str[i])
 	{
-		n = is_n(str, 0);
-		ft_putstr_fd((str + n), 1);
+		while (is_n(str[i]))
+		{
+			n = 0;
+			i++;
+		}
+		if (str[i])
+		{
+		while (str[i])
+		{
+				ft_putstr_fd(str[i],1);
+				i++;
+				if (str[i])
+					ft_putstr_fd(" ", 1);
+			}
+		}
 	}
-	if (n == 0)
+	if (n == 1)
 		ft_putstr_fd("\n", 1);
-	s()->sig->ret = 0;
-	return ;
 }
--- srcs/trimtrim/trimer.c
+++ srcs/trimtrim/trimer.c
@@ -41,8 +41,7 @@ void	supp_dollarz(t_shell *shell, t_lexer *l, int *i , t_List st)
 	start = ft_strldup(l->str, (*i));
 	finish = NULL;
 	if (l->str[fin] == '?' && fin++)
-		ret = ft_itoa(s()->sig->ret);
-	else
+		ret = ft_itoa(s()->sig->ret);	else
 	{
 		while ((ft_isalnum(l->str[fin]) || (l->str[fin] == '_')) && l->str[fin])
 		{
