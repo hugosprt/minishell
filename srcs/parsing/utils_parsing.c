c0846c566447fa9cae848f35f40ff20d24625b32
rpol <polrotyuni@gmail.com>
2022-09-07 16:01:11 +0200

    fixed pipe added make fd great again

--- srcs/parsing/utils_parsing.c
+++ srcs/parsing/utils_parsing.c
@@ -1,72 +1,48 @@
 #include "../../includes/minishell.h"
 
-static int	last_exec(t_shell *s)
+
+void	make_the_fd_great_again(t_shell *sh)
 {
-	if (s->parsing->block[0] == 0)
-	{
-		dup2(s->parsing->prev_in, STDIN_FILENO);
-		if (s->parsing->prev_in != 0)
-			close(s->parsing->prev_in);
-	}
-	// ft_putstr_fd("last exec", 2);
-	// write(2, "___\n", 4);
-	exec(s);
-	dup2(s->parsing->std_in, STDIN_FILENO);
-	dup2(s->parsing->std_out, STDOUT_FILENO);
-	close(s->parsing->std_out);
-	return (0);
+	sh->parsing->std_in = dup(STDIN_FILENO);
+	sh->parsing->std_out = dup(STDOUT_FILENO);
+	dup2(sh->parsing->prev_in, STDIN_FILENO);
+	if (sh->parsing->prev_in != 0)
+		close(sh->parsing->prev_in);
+	if (!sh->parsing->nb_pipe)
+		return ;
+	sh->parsing->nb_pipe--;
+	pipe(sh->parsing->pipe);
+	dup2(sh->parsing->pipe[1], STDOUT_FILENO);
+	close(sh->parsing->pipe[1]);
+	sh->parsing->prev_in = dup(sh->parsing->pipe[0]);
+	close(sh->parsing->pipe[0]);
 }
 
-static t_lexer	*mario(t_shell *s)
-{
-	//write(2, "ici\n", 4);
-	if (s->parsing->block[0] == 0)
-	{
-		dup2(s->parsing->prev_in, STDIN_FILENO);
-		if (s->parsing->prev_in != 0)
-			close(s->parsing->prev_in);
-	}
-	if (s->parsing->block[1] == 1)
-	{
-		pipe(s->parsing->pipe);
-		dup2(s->parsing->pipe[STDOUT_FILENO], STDOUT_FILENO);
-		close(s->parsing->pipe[STDOUT_FILENO]);
-		s->parsing->prev_in = dup(s->parsing->pipe[STDIN_FILENO]);
-		close(s->parsing->pipe[STDIN_FILENO]);
-	}
-	exec(s);
-	dup2(s->parsing->std_in, STDIN_FILENO);
-	close(s->parsing->std_in);
-	dup2(s->parsing->std_out, STDOUT_FILENO);
-	close(s->parsing->std_out);
-//	write(2, "ic2\n", 4);
-	s->parsing->l = s->parsing->l->next;
-	return (s->parsing->l);
-}
-
-int	make_block(t_shell *s)
+int	make_block(t_shell *sh)
 {
 	t_parsing	*p;
 
-	p = s->parsing;
-	if (p->l->koi == ARG)
+	p = sh->parsing;
+	make_the_fd_great_again(sh);
+	while (p->l->koi != END && p->l->koi != PIPE)
 	{
-		p->arg = add2tab(p->arg, p->l->str);
-		p->l = p->l->next;
+		if (p->l->koi == ARG)
+		{
+			p->arg = add2tab(p->arg, p->l->str);
+			p->l = p->l->next;
+		}
+		if (p->l->koi == R_REDIR || p->l->koi == RR_REDIR || p->l->koi == L_REDIR)
+			p->l = redir(p);
 	}
-	//write(2, "icw\n", 4);
-	if (p->l->koi == R_REDIR || p->l->koi == RR_REDIR || p->l->koi == L_REDIR)
-		p->l = redir(p);
-	s->parsing = p;
+	exec(sh);
+	dup2(sh->parsing->std_in, STDIN_FILENO);
+	close(sh->parsing->std_in);
+	dup2(sh->parsing->std_out, STDOUT_FILENO);
+	close(sh->parsing->std_out);
 	if (p->l->koi == PIPE)
-		p->l = mario(s);
-	if (p->l->koi == END)
 	{
-		last_exec(s);
-		s->parsing->l->koi = 10;
-		return (0);
+		sh->parsing->l = p->l->next;
+		make_block(sh);
 	}
-	if (p->l->koi != 10)
-		make_block(s);
 	return (0);
 }
