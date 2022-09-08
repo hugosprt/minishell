9274ff33587ce2e5853e07da2ff9bcbdcd08b6cc
Ubuntu <ubuntu@ip-172-31-31-104.eu-west-3.compute.internal>
2022-09-08 10:32:56 +0000

    la moulaga

--- includes/fonctions.h
+++ includes/fonctions.h
@@ -95,5 +95,5 @@ void		quote_problem(t_shell *shell, int error_value);
 void		get_signal(int sig);
 void		signal_gestion(t_shell *s);
 void		ctrl_d_exit(void);
-
+void	path_not_set(t_parsing *p);
 #endif
