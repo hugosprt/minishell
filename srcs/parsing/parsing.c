3e73894302ed7590a01402e14fc20098d68602a9
rpol <polrotyuni@gmail.com>
2022-10-08 23:30:02 +0200

    fixed redir error message if there is a heredoc after

--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -6,27 +6,47 @@
 /*   By: rpol <rpol@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 13:36:14 by rpol              #+#    #+#             */
-/*   Updated: 2022/10/08 15:41:42 by rpol             ###   ########.fr       */
+/*   Updated: 2022/10/08 23:24:37 by rpol             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+int	herenext(t_shell *sh)
+{
+	t_lexer	*l;
+
+	l = sh->parsing->l;
+	if (l->koi == ARG)
+	{
+		while (l->koi != PIPE && l->koi != END)
+		{
+			if (l->koi == LL_REDIR)
+			{
+				sh->tmp = sh->parsing->l->str;
+				return (0);
+			}
+			l = l->next;
+		}
+	}
+	return (1);
+}
+
 int	error(t_shell *sh, int i)
 {
-	ft_putstr_fd("minishell: ", 2);
 	sh->error = 1;
 	s()->sig->ret = 1;
 	if (i == 1)
 	{
+		ft_putstr_fd("minishell: ", 2);
 		ft_putstr_fd("MALLOC ERROR\n", 2);
 		ft_exit(0, NULL);
 	}
-	else
+	else if (herenext(sh))
 	{
+		ft_putstr_fd("minishell: ", 2);
 		ft_putstr_fd(sh->parsing->l->str, 2);
 		ft_putstr_fd(": No such file or directory\n", 2);
-		sh->error = 2;
 		if (i == 3)
 			exit(130);
 	}
