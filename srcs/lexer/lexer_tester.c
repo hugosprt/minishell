b20180fe235d4454322fa6e0998992a90f6d66c5
Hugo SPRIET <hspriet@e1r3p21.clusters.42paris.fr>
2022-10-09 12:41:27 +0200

    yo

--- srcs/lexer/lexer_tester.c
+++ srcs/lexer/lexer_tester.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lexer_tester.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rpol <rpol@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: hspriet <hspriet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 13:41:30 by rpol              #+#    #+#             */
-/*   Updated: 2022/10/03 13:44:02 by rpol             ###   ########.fr       */
+/*   Updated: 2022/10/09 12:40:38 by hspriet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,19 +32,18 @@ int	first_arg(t_shell *s, t_lexer *l)
 	t_lexer	*tmp;
 
 	tmp = l;
-	if (tmp->koi == PIPE && (tmp->next->koi == ARG || tmp->next->koi == PIPE))
-		syntax_problem(s, 0);
 	if (size_list(tmp) == 2)
 	{
 		if (tmp->koi > 0)
 			syntax_problem(s, 5);
 	}
-	else if (tmp->koi == PIPE && (tmp->next->koi == ARG
-			|| tmp->next->koi == PIPE))
-		syntax_problem(s, 0);
+	if (tmp->koi == RR_REDIR && tmp->next->koi == R_REDIR)
+		syntax_problem(s, 2);
 	else
 	{
 		tmp = tmp->next;
+		if (find_last_elem(tmp) == PIPE)
+			syntax_problem(s, 0);
 		while (tmp && tmp->next && !s->error)
 		{
 			test_pipe(s, tmp);
@@ -91,23 +90,26 @@ int	test_pipe(t_shell *s, t_lexer *l)
 	t_lexer	*tmp;
 
 	tmp = l;
-	if (find_last_elem(tmp) == PIPE)
-		syntax_problem(s, 0);
 	if (tmp->prev->koi == R_REDIR && tmp->koi == PIPE)
 		syntax_problem(s, 0);
-	if (tmp->koi == L_REDIR && tmp->next->koi == END)
+	else if (tmp->koi == L_REDIR && tmp->next->koi == END)
 		syntax_problem(s, 5);
-	if (tmp->prev->koi == L_REDIR && tmp->koi == PIPE)
+	else if (tmp->prev->koi == L_REDIR && tmp->koi == PIPE)
 		syntax_problem(s, 0);
-	if (tmp->prev->koi == 5 && tmp->koi == 2 && tmp->next->koi == 2)
+	else if (tmp->prev->koi == RR_REDIR && tmp->koi == R_REDIR)
 		syntax_problem(s, 0);
-	if (tmp->koi == PIPE && tmp->next->koi == PIPE)
-		return (0);
-	if (tmp->koi == LL_REDIR && tmp->next->koi == END)
+	else if (tmp->prev->koi == PIPE && tmp->koi == PIPE
+		&& tmp->next->koi == PIPE)
+		syntax_problem(s, 0);
+	else if (tmp->koi == LL_REDIR && tmp->next->koi == END)
 		syntax_problem(s, 5);
-	if (tmp->koi == R_REDIR && tmp->next->koi == END)
+	else if (tmp->koi == R_REDIR && tmp->next->koi == END)
 		syntax_problem(s, 5);
-	if (tmp->koi == RR_REDIR && tmp->next->koi == END)
+	else if (tmp->prev->koi == R_REDIR && tmp->koi == L_REDIR)
+		syntax_problem(s, 5);
+	else if (tmp->prev->koi == L_REDIR && tmp->koi == R_REDIR)
+		syntax_problem(s, 5);
+	else if (tmp->koi == RR_REDIR && tmp->next->koi == END)
 		syntax_problem(s, 5);
 	return (1);
 }
